21388ff521d7a71f2de75a19957f05f8882e8a07
hongxun.li <hongxun.li@ge.com>
2018-01-17 14:12:22 +0800

    add HMR in style-loader

--- build/style-loader/add-style.js
+++ build/style-loader/add-style.js
@@ -2,4 +2,5 @@ module.exports = function (content) {
   var style = document.createElement("style")
   style.innerHTML = content
   document.head.appendChild(style)
+  return style
 }
--- build/style-loader/index.js
+++ build/style-loader/index.js
@@ -8,8 +8,16 @@ module.exports = function (source) {}
  */
 module.exports.pitch = function (request) {
   var result = [
-    'var content=require(' + loaderUtils.stringifyRequest(this, '!!' + request) + ')',
-    'require(' + loaderUtils.stringifyRequest(this, '!' + path.join(__dirname, "add-style.js")) + ')(content)',
+    'var content=require(' + loaderUtils.stringifyRequest(this, '!!' + request) + ');',
+    'var style = require(' + loaderUtils.stringifyRequest(this, '!' + path.join(__dirname, "add-style.js")) + ')(content);',
+    'if (module.hot) {',
+    '  if (!content.locals) {', // 未启用 css modules， 则可以直接更新 style内容即可。如果启用了，因为还需要父组件更新，所以这里就不作处理，直接冒泡到父组件处理（style-loader被父组件重新调用了一次）
+		'		module.hot.accept(' + loaderUtils.stringifyRequest(this, '!!' + request) + ', function() {',
+    '     style.innerHTML = require(' + loaderUtils.stringifyRequest(this, '!!' + request) + ');',
+    '   })',
+    '  }',
+    '  module.hot.dispose(function () { console.log(style);style.remove() })',
+    '}',
     'if(content.locals) module.exports = content.locals'
   ]
   return result.join(';')
